f08f2383a34b2d623aee908d9a63c37638cb6339
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-09-21 11:55:13 +0300

    REFACTOR: Move `CategorySelectionDelegate` to view model; Get rid of view model's optionality by setting it during view controller's initialization

--- Tracker/Tracker/TrackerCreation/NewCategory/NewCategoryViewController.swift
+++ Tracker/Tracker/TrackerCreation/NewCategory/NewCategoryViewController.swift
@@ -2,8 +2,7 @@ import UIKit
 
 final class NewCategoryViewController: UIViewController {
     // MARK: - Properties
-    private var viewModel: NewCategoryViewModel?
-    private weak var delegate: CategorySelectionDelegate?
+    private var viewModel: NewCategoryViewModelProtocol
     
     // MARK: - UI Elements
     private lazy var stubImage: UIImageView = {
@@ -54,23 +53,35 @@ final class NewCategoryViewController: UIViewController {
         return button
     }()
     
+    // MARK: - Initialization
+    init(viewModel: NewCategoryViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    convenience init() {
+        self.init(viewModel: NewCategoryViewModel())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        viewModel = NewCategoryViewModel()
         bindViewModel()
-        
         configureUI()
     }
     
     // MARK: - Binding
     private func bindViewModel() {
-        viewModel?.categoriesUpdated = { [weak self] in
+        viewModel.categoriesUpdated = { [weak self] in
             self?.categoriesTableView.reloadData()
         }
         
-        viewModel?.emptyStateChanged = { [weak self] isEmpty in
+        viewModel.emptyStateChanged = { [weak self] isEmpty in
             if isEmpty {
                 self?.configureStubImageAndText()
             } else {
@@ -95,7 +106,7 @@ final class NewCategoryViewController: UIViewController {
     
     // MARK: - Public Methods
     func setDelegate(delegate: CategorySelectionDelegate) {
-        self.delegate = delegate
+        viewModel.setDelegate(delegate: delegate)
     }
     
     // MARK: - Actions
@@ -111,7 +122,7 @@ final class NewCategoryViewController: UIViewController {
 // MARK: - UITableViewDataSource
 extension NewCategoryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel?.categories.count ?? 0
+        return viewModel.categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -120,10 +131,10 @@ extension NewCategoryViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         
-        if let category = viewModel?.categories[indexPath.row] {
-            cell.setTitleLabel(to: category.categoryName)
-            cell.setCheckmarkVisible(category.categoryName == viewModel?.selectedCategory?.categoryName)
-        }
+        let category = viewModel.categories[indexPath.row]
+        cell.setTitleLabel(to: category.categoryName)
+        cell.setCheckmarkVisible(category.categoryName == viewModel.selectedCategory?.categoryName)
+        
         
         if indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
             cell.roundLowerCorners()
@@ -151,18 +162,14 @@ extension NewCategoryViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        viewModel?.selectCategory(at: indexPath.row)
-        if let selectedCategory = viewModel?.selectedCategory {
-            delegate?.didSelectCategory(selectedCategory)
-        }
+        viewModel.selectCategory(at: indexPath.row)
     }
 }
 
 // MARK: - CategoryCreationDelegate
 extension NewCategoryViewController: CategoryCreationDelegate {
     func didCreateCategory(_ category: TrackerCategory) {
-        viewModel?.createCategory(category)
-//        removeStubImageAndText()
+        viewModel.createCategory(category)
     }
 }
 
--- Tracker/Tracker/TrackerCreation/NewCategory/NewCategoryViewModel.swift
+++ Tracker/Tracker/TrackerCreation/NewCategory/NewCategoryViewModel.swift
@@ -1,7 +1,21 @@
 import Foundation
 
-final class NewCategoryViewModel {
+// MARK: - Protocol
+protocol NewCategoryViewModelProtocol {
+    var categories: [TrackerCategory] { get }
+    var selectedCategory: TrackerCategory? { get }
+    var categoriesUpdated: (() -> Void)? { get set }
+    var emptyStateChanged: ((Bool) -> Void)? { get set }
+    func createCategory(_ category: TrackerCategory)
+    func selectCategory(at index: Int)
+    func setDelegate(delegate: CategorySelectionDelegate)
+}
+
+final class NewCategoryViewModel: NewCategoryViewModelProtocol {
+    
+    // MARK: - Properties
     private var trackerCategoryStore: TrackerCategoryStore
+    private weak var delegate: CategorySelectionDelegate?
     private(set) var categories: [TrackerCategory] = [] {
         didSet {
             categoriesUpdated?()
@@ -17,16 +31,19 @@ final class NewCategoryViewModel {
     var categoriesUpdated: (() -> Void)?
     var emptyStateChanged: ((Bool) -> Void)?
     
+    // MARK: - Initialization
     init(trackerCategoryStore: TrackerCategoryStore = TrackerCategoryStore()) {
         self.trackerCategoryStore = trackerCategoryStore
         loadCategories()
     }
     
+    // MARK: - Private Methods
     private func loadCategories() {
         categories = trackerCategoryStore.readTrackerCategories()
         emptyStateChanged?(categories.isEmpty)
     }
     
+    // MARK: - Protocol Implementation
     func createCategory(_ category: TrackerCategory) {
         trackerCategoryStore.createTrackerCategory(category)
         categories.append(category)
@@ -37,8 +54,13 @@ final class NewCategoryViewModel {
         let category = categories[index]
         if category.categoryName != selectedCategory?.categoryName {
             selectedCategory = category
+            delegate?.didSelectCategory(category)
         } else {
             selectedCategory = nil
         }
+    }
+    
+    func setDelegate(delegate: CategorySelectionDelegate) {
+        self.delegate = delegate
     }
 }
